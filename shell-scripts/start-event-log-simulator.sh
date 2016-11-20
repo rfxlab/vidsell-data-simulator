3d43a30cdd2beb66300bec95ccfec1bd3983bc91
trieu <tantrieuf31@gmail.com>
2016-11-20 20:46:51 +0700

    shell script to start code

---
+++ shell-scripts/start-event-log-simulator.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+JVM_PARAMS="-Xms512m -Xmx2048m -XX:+TieredCompilation -XX:+UseCompressedOops -XX:+DisableExplicitGC -XX:+UseNUMA -server"
+JAR_NAME="vidsell-data-simulator-bot-1.0.jar"
+
+/usr/bin/java -jar $JVM_PARAMS $JAR_NAME  >> /dev/null 2>&1 &
---
+++ shell-scripts/stop-event-log-simulator.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+kill -9 $(pgrep -f vidsell-data-simulator-bot-1.0.jar)
+sleep 1
